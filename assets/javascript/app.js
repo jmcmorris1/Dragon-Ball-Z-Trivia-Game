d55ba714ba3799b69b7e87bf8536f09fbc5bd871
Jamyl Ali <Jamyl Ali>
2017-03-03 14:13:48 -0500

    First Trivia Game

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+$("#start-button").ready(function()
+
+	var number = 50;
+    $("#start-button").on("click", start); 
+    $("#results-button").on("click", results);  
+    $("#rematch-button").on("click", rematch); 
